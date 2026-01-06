277f64afe950fdc7a60880e7619c5aefe5b84c86
Nizo-Developer <nizo5671@gmail.com>
2026-01-06 14:22:49 +0700

    v1.0.9

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
     origin: "https://nuzulizzati994-web.github.io",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
