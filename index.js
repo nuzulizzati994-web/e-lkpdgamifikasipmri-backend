a069d35a2c4747bb699fb3b7f3063f36191f51c4
Nizo-Developer <nizo5671@gmail.com>
2026-01-06 11:38:11 +0700

    v1.0.4

--- index.js
+++ index.js
@@ -5,32 +5,27 @@ const routes = require("./src/routes");
 const sequelize = require("./src/config/db");
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// ✅ Izinkan akses dari semua origin (bisa disesuaikan)
 app.use(
   cors({
-    origin: "*", // ganti "*" dengan alamat IP tertentu kalau mau lebih aman, misal: "http://192.168.1.5:8080"
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: "https://nuzulizzati994-web.github.io/*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use(express.json());
-
 app.use("/api", routes);
 
 sequelize
-  .sync({ alter: true, logging: console.log })
-  // .sync({ force: true })
-  .then(async () => {
-    console.log("🧩 Database siap disinkronkan!");
-
-    app.listen(port, "0.0.0.0", () => {
-      console.log(`✅ Server berjalan di jaringan pada port ${port}`);
-    });
+  .sync()
+  .then(() => {
+    console.log("Database connected");
+    app.listen(port, "0.0.0.0", () =>
+      console.log(`Server running on port ${port}`)
+    );
   })
-  .catch((err) => {
-    console.error("❌ Gagal sinkronisasi database:", err.message);
-  });
+  .catch(console.error);
